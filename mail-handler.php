578ac9dae98787b49092a0b9e092a1066fcc575a
brentvanhese <r0903817@student.thomasmore.be>
2021-12-20 14:01:51 +0100

    mail php aanpassen

--- mail-handler.php
+++ mail-handler.php
@@ -2,8 +2,8 @@
 if(isset($_POST['submit'])){
     $to = "vanhesebrent@gmail.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
+    $first_name = $_POST['firstname'];
+    $last_name = $_POST['lastname'];
     $subject = "Form submission";
     $subject2 = "Copy of your form submission";
     $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
@@ -15,7 +15,6 @@ if(isset($_POST['submit'])){
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
     echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
-    // You cannot use header and echo together. It's one or the other.
     }
 ?>
 <!--https://stackoverflow.com/questions/18379238/send-email-with-php-from-html-form-on-submit-with-the-same-script-->
